5f91761fdf0933c2062efd9e3dd14c6182c52c5e
Shantanu Debnath <sdnath017@gmail.com>
2017-06-17 02:11:56 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,14 @@
 
     jQuery(document).ready(function($){
 
-
+			$('.logo-carousel').owlCarousel({
+				items:6,
+				margin:30,
+				dots:false,
+			    loop:true,
+			    nav:false,
+			    autoplay:true,
+			});
 			$('.case-studies-carousel').owlCarousel({
 				items:3,
 				margin:30,
@@ -19,15 +26,13 @@
 			    loop:true,
 			    nav:false,
 			});
-	    
-			$('.logo-carousel').owlCarousel({
-				items:6,
-				margin:30,
-				dots:false,
-			    loop:true,
-			    nav:false,
-			    autoplay:true,
-			});
+			$('.single-testimonial').owlCarousel({
+							items:1,
+							margin:30,
+							dots:true,
+						    loop:true,
+						    nav:false,
+						});
 
 
         
